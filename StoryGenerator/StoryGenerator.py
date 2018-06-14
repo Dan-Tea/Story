49d245e2ab611ead82c586bff7fec0d889abf66a
Daniel Teape <DanielPaul.T@hotmail.co.uk>
2018-06-14 13:30:26 +0100

    Characters + speech

--- StoryGenerator/StoryGenerator.py
+++ StoryGenerator/StoryGenerator.py
@@ -1,1 +1,19 @@
+woman = []
+man = []
+place = []
+he_said = []
+she_said = []
+consequence = []
 
+for i in range(0,3):
+    female_character = input("Input female character: ")
+    female_speech = input("What did she say? : ")
+    woman.append(female_character)
+    she_said.append(female_speech)
+
+
+for i in range(0,3):
+    male_character = input("Input male character: ")
+    male_speech = input("What did he say? : ")
+    man.append(male_character)
+    he_said.append(male_speech)
